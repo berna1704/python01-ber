6139979bb07e4370ac9ed481e96c7c13ca315952
berna1704 <bernardo.batista@escola.pr.gov.br>
2024-03-27 10:59:27 -0300

    first commit

---
+++ aula02.py
@@ -0,0 +1,13 @@
+import os
+#print("Ola mundo")
+
+print("0 3DS e a turma número 1 do sagrada")
+turma="3DS"
+pos=1
+
+print("O",turma,"é a turma número",pos,"do sagrada")
+
+print(type(turma))
+print(type(pos))
+
+os.system("")
